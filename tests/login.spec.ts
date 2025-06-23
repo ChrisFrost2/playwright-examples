d016f9d97cb81866028e5ec9ad0db8f20cbc38a6
ChrisFrost2 <kampa@interia.pl>
2025-06-23 21:39:35 +0200

    fix

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -37,24 +37,24 @@ test.describe('Login tests', () => {
         });
     });
 
-    test.skip('Failed login - user not authorired because is locked out', async ({ page, loginPage }) => {
+    test.skip('Failed login - user not authorized because is locked out', async ({ page, loginPage }) => {
         await loginPage.login('locked_out_user', 'secret_sauce');
         await loginPage.errorMessagePresented('Sorry, this user has been locked out.');
         // test.info().annotations.push({ type: 'issue', description: 'Login failed as expected!' });    
     });
 
-    test.skip('Failed login - user not authorired and still on login page when passing wrong login or password', async ({ page, loginPage }) => {
+    test.skip('Failed login - user not authorized and still on login page when passing wrong login or password', async ({ page, loginPage }) => {
         await loginPage.login('standard_user', 'wrong_password');
         await loginPage.errorMessagePresented('Username and password do not match any user in this service');
         // test.info().annotations.push({ type: 'issue', description: 'Login failed as expected!' });    
     });
 
-    test.skip('Failed login - user not authorired and still on login page when did not pass password', async ({ page, loginPage }) => {
+    test.skip('Failed login - user not authorized and still on login page when did not pass password', async ({ page, loginPage }) => {
         await loginPage.login('standard_user', '');
         await loginPage.errorMessagePresented('Password is required');
     });
 
-    test.skip('Failed login - user not authorired and still on login page when did not pass username', async ({ page, loginPage }) => {
+    test.skip('Failed login - user not authorized and still on login page when did not pass username', async ({ page, loginPage }) => {
         await loginPage.password_input.fill('wrong_password');
         await loginPage.login_button.click();
         await loginPage.errorMessagePresented('Username is required');
